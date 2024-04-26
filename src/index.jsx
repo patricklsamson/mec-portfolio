d401c49e8beed8b4330f786bb02ed0a849f17dde
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2024-04-26 12:08:32 +0800

    Add gsap

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 import { Home } from "./pages";
 import "./assets/css/index.css";
 
+gsap.registerPlugin(useGSAP);
 createRoot(document.getElementById("root")).render(<Home />);
